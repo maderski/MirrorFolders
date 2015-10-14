2bd774afb791f924cb743b91f7ff3da4b8024731
Jason <jason@internetthings.io>
2015-10-14 18:12:27 -0400

    Added comments

--- MirrorFolders.py
+++ MirrorFolders.py
@@ -7,38 +7,44 @@ import subprocess
 import re
 
 
+# Folder related tasks are here
 class Folders:
 
     sourcefolder = "Nothing"
     destinationfolder = "Nothing"
 
+    # Brings up GUI to select the source folder and stores choice in the variable sourcefolder
     def pickSourceFolder(self, root):
-        # root.withdraw()
         self.sourcefolder = tkFileDialog.askdirectory(parent=root, title='Choose SOURCE folder')
         print self.sourcefolder
 
+    # Brings up GUI to select the destination folder and stores choice in the variable destinationfolder
     def pickDestinationFolder(self, root):
-        # root.withdraw()
         self.destinationfolder = tkFileDialog.askdirectory(parent=root, title='Choose DESTINATION folder')
         print self.destinationfolder
 
-
+# Batchfile related tasks are here
 class BatchFile:
 
     previousfolders = "None"
 
+    # Gets the current location of the program, currently not used
     def getCurrentlocation(self):
         return os.getcwd()
 
+    # Checks to see if a batchfile exists, if batchfile with inputted name exists then it returns 'true'
     def batchFileDoesExist(self, batchfilename):
         return os.path.isfile(batchfilename)
 
+    # Creates a batch file that uses the built in windows tool robocopy
     def writeBatchFile(self, batchfilename, source, destination):
         outputfile = open(batchfilename, "w")
         outputfile.write("@echo off" + "\n"
                          + "robocopy " + source + " " + destination + " " + "/MIR")
         outputfile.close()
 
+    # Reads existing batchfile if it exists and stores the source and destination directories in the variable previous
+    # folders.  If file does exist, it creates a batchfile with placeholders for both source and destination directories
     def readBatchFile(self, batchfilename):
         if self.batchFileDoesExist(batchfilename):
             try:
@@ -60,20 +66,25 @@ class BatchFile:
             self.writeBatchFile(batchfilename, "None", "None")
             self.readBatchFile(batchfilename)
 
+    # Runs the batchfile
     def runBatchFile(self, filename):
         runBatch = subprocess.Popen(filename)
         stdout, stderr = runBatch.communicate()
 
-
+# All GUI relate stuff is here
 class GUI:
 
+    # Create the GUI window with buttons, text, etc...
     def createWindow(self):
 
         root = Tk()
         f = Folders()
         bf = BatchFile()
 
+        # Name of the batchfile
         nameofbatchfile = "runRobocopy.bat"
+
+        # Get prewiously used Directories
         bf.readBatchFile(nameofbatchfile)
 
         f.sourcefolder = bf.previousfolders[0]
@@ -81,8 +92,10 @@ class GUI:
         sourcelabeltext = StringVar()
         destinationlabeltext = StringVar()
 
+        # Window title
         root.title("Easy Mirror of two folders")
 
+        # Source Directory text display
         sourcelabeltext.set(f.sourcefolder)
         sourceLabel = Label(root,
                             fg="saddle brown",
@@ -90,6 +103,7 @@ class GUI:
                             textvariable=sourcelabeltext)
         sourceLabel.pack(padx=0, pady=(10, 10))
 
+        # Button to launch window to choose source
         sourceButton = Button(root,
                               bg="khaki",
                               text='Select Source',
@@ -99,6 +113,7 @@ class GUI:
                               command=lambda: self.sourceButtonClicked(root, f, bf, nameofbatchfile, sourcelabeltext))
         sourceButton.pack(padx=20, pady=0)
 
+        # Destination Directory text display
         destinationlabeltext.set(f.destinationfolder)
         destinationLabel = Label(root,
                                  fg="dark green",
@@ -106,6 +121,7 @@ class GUI:
                                  textvariable=destinationlabeltext)
         destinationLabel.pack(padx=0, pady=(50, 10))
 
+        # Button to launch windo to choose destination
         destinationButton = Button(root,
                                    fg="white",
                                    bg="olive drab",
@@ -116,6 +132,7 @@ class GUI:
                                    command=lambda: self.destinationButtonClicked(root, f, bf, nameofbatchfile, destinationlabeltext))
         destinationButton.pack(padx=20, pady=0)
 
+        # Button to run Batchfile
         runButton = Button(root,
                            fg="white",
                            bg="dark red",
@@ -127,25 +144,30 @@ class GUI:
         runButton.pack(padx=0, pady=(30, 30))
         root.mainloop()
 
+    # Actions for when sourcebutton is clicked
     def sourceButtonClicked(self, root, folders, batchfile, bfname, text):
         folders.pickSourceFolder(root)
         batchfile.writeBatchFile(bfname, folders.sourcefolder, folders.destinationfolder)
         text.set(folders.sourcefolder)
 
+    # Action for when destinationbutton is clicked
     def destinationButtonClicked(self, root, folders, batchfile, bfname, text):
         folders.pickDestinationFolder(root)
         batchfile.writeBatchFile(bfname, folders.sourcefolder, folders.destinationfolder)
         text.set(folders.destinationfolder)
 
+    # Action for when runbutton is clicked
     def runButtonClicked(self, batchfile, bfname):
         batchfile.runBatchFile(bfname)
 
 
+# Run the program
 class main:
 
     def __init__(self):
         pass
 
+    # Program is run here
     def start(self):
         g = GUI()
         g.createWindow()
